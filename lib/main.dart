460738f48a552cd7f3698d5ef7f4d200ea7ffee0
PHA PHANNA <phaphanna55@gmail.com>
2022-05-03 20:22:39 +0700

    ff

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text("Hello GetX"),
         actions: [
           IconButton(
               onPressed: () {
